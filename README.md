e1ce2d957637187ef7291cbb26cbdef7d36eb18c
soli-soli <zergrage@gmail.com>
2023-04-17 21:47:07 +0900

    Updated to support 1.4

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
-# LetThemEatPaste
+# Let Them Eat Paste
 A Rimworld mod that makes colonists prefer to feed prisoners nutrient paste meals instead of better meals that were intended for colonists
 
-### Download
+---
 
-**Last update:** 1/29/2018 (updated to `18.0`)
+**This mod is originally made by Unislash and I updated it to be compatible with the latest version(1.4)**
 
-[Latest release](https://github.com/Unislash/LetThemEatPaste/releases/tag/18.0)
-
-[All the releases](https://github.com/Unislash/LetThemEatPaste/releases)
+[Link to the original mod](https://steamcommunity.com/sharedfiles/filedetails/?id=1285321346)
 
 ### License
 
-This mod can be used by anyone for any purpose as long as it's free and there's proper attribution - mod name, author's name and a link to this page.
+This mod can be used by anyone for any purpose as long as it's free and there's proper attribution - mod name, author's name(Unislash) and a link to the original mod.
--- Source/LetThemEatPaste/LetThemEatPaste/HarmonyPatches.cs
+++ Source/LetThemEatPaste/LetThemEatPaste/HarmonyPatches.cs
@@ -1,4 +1,4 @@
-using Harmony;
+using HarmonyLib;
 using RimWorld;
 using System.Collections.Generic;
 using Verse;
@@ -7,54 +7,52 @@ using System;
 using System.Reflection;
 
 namespace LetThemEatPaste {
-
     [StaticConstructorOnStartup]
     static class Patches {
-
         static Patches() {
-            HarmonyInstance harmony = HarmonyInstance.Create("com.unislash.LetThemEatPaste.patches");
+            Harmony harmony = new Harmony("com.unislash.LetThemEatPaste.patches");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
 
-        public static Thing getPasteSource(Pawn getter, Pawn eater) {
+        public static Thing GetPasteSource(Pawn getter, Pawn eater) {
             Thing chosenFood = null;
 
-            // If the eater is null, nothing to do
-            if (eater == null) {
+            // 1. If the eater is null, nothing to do
+            // 2. If the eater isn't a prisoner, use the original behavior
+            // 3. If the eater is also the getter, then the prisoner is getting food for themselves,
+            //      so we want to use the original behavior
+            if (eater == null || !eater.IsPrisoner || eater == getter) {
                 return null;
             }
 
-            // If the eater isn't a prisoner, use the original behavior
-            if (!eater.IsPrisoner) {
-                return null;
-            }
-
-            // If the eater is also the getter, then the prisoner is getting food for themselves, so we want to use the original behavior
-            if (eater == getter) {
-                return null;
-            }
-            
             // Find any nutrient paste meals already made
-            List<Thing> possibleThings = getter.Map.listerThings.ThingsInGroup(ThingRequestGroup.FoodSourceNotPlantOrTree);
-            Comparison<Thing> comparison = delegate(Thing t1, Thing t2) {
+            List<Thing> possibleThings =
+                getter.Map.listerThings.ThingsInGroup(ThingRequestGroup.FoodSourceNotPlantOrTree);
+
+            int Comparison(Thing t1, Thing t2) {
                 float num = (float) (t1.Position - getter.Position).LengthHorizontalSquared;
                 float value = (float) (t2.Position - getter.Position).LengthHorizontalSquared;
                 return num.CompareTo(value);
-            };
-            possibleThings.Sort(comparison);
+            }
+
+            possibleThings.Sort(Comparison);
 
             TraverseParms traverseParams = TraverseParms.For(getter, Danger.Deadly, TraverseMode.ByPawn, false);
 
             // Go find the closest nutrient paste meal or dispenser
             foreach (var thing in possibleThings) {
-                if (thing.def.defName == "MealNutrientPaste" && !thing.IsForbidden(getter) && getter.CanReserve(thing, 1, -1, null, false)
-                && getter.Map.reachability.CanReach(getter.Position, thing, PathEndMode.ClosestTouch, traverseParams)) {
+                if (thing.def.defName == "MealNutrientPaste" && !thing.IsForbidden(getter) &&
+                    getter.CanReserve(thing, 1, -1, null, false)
+                    && getter.Map.reachability.CanReach(getter.Position, thing, PathEndMode.ClosestTouch,
+                        traverseParams)) {
                     chosenFood = thing;
                     break;
                 }
-                Building_NutrientPasteDispenser building_NutrientPasteDispenser = thing as Building_NutrientPasteDispenser;
-                if (building_NutrientPasteDispenser != null && building_NutrientPasteDispenser.CanDispenseNow
-                    && getter.Map.reachability.CanReach(getter.Position, thing, PathEndMode.ClosestTouch, traverseParams)) {
+
+                if (thing is Building_NutrientPasteDispenser building_NutrientPasteDispenser
+                    && building_NutrientPasteDispenser.CanDispenseNow
+                    && getter.Map.reachability.CanReach(getter.Position,
+                        thing, PathEndMode.ClosestTouch, traverseParams)) {
                     chosenFood = thing;
                     break;
                 }
@@ -63,14 +61,13 @@ namespace LetThemEatPaste {
             // Return the chosen food
             return chosenFood;
         }
-        
-        [HarmonyPatch(typeof(FoodUtility))]
-        [HarmonyPatch("TryFindBestFoodSourceFor")]
-        static class TryFindBestFoodSourceFor_Patch {
 
+        [HarmonyPatch(typeof(FoodUtility))]
+        [HarmonyPatch("TryFindBestFoodSourceFor_NewTemp")]
+        public static class TryFindBestFoodSourceFor_Patch {
             /**
-             * Force wardens to not use meals in their inventory when feeding prisoners in an effort to prevent them from using food they would use
-             * themselves.
+             * Force wardens to not use meals in their inventory when feeding prisoners
+             *  in an effort to prevent them from using food they would use themselves.
              */
             static bool Prefix(Pawn getter, Pawn eater, ref bool canUseInventory) {
                 // If the eater is null, nothing to do
@@ -79,49 +76,48 @@ namespace LetThemEatPaste {
                 }
 
                 // If the eater isn't a prisoner, use the original behavior
-                if (!eater.IsPrisoner) {
+                // Or if the eater is also the getter, then the prisoner is getting food for themselves,
+                //  so we want to use the original behavior
+                if (!eater.IsPrisoner || eater == getter) {
                     return true;
                 }
-                
-                // If the eater is also the getter, then the prisoner is getting food for themselves, so we want to use the original behavior
-                if (eater == getter) {
-                    return true;
-                }
-                
-                // Ok, so now that we've identified that we're a colonist feeding a prisoner, we don't want to use any amazing foods in our inventory,
-                // unless...
-                // 1) The best food in the inventory could actually be a nutrient paste meal. If so, feed that to the prisoner scum!
-                // 2) We might not be able to find any paste meals or dispensers. If so, then I guess we can feed the prisoner what we have in
-                //    our inventory... (but next time we're totally going to feed them paste)
-                Thing foodInInventory = FoodUtility.BestFoodInInventory(getter, null, FoodPreferability.MealAwful, FoodPreferability.MealLavish, 0f, false);
-                Thing canFindPaste = getPasteSource(getter, eater);
-                if (foodInInventory != null && foodInInventory.def.defName != "MealNutrientPaste" && canFindPaste != null) {
+
+                // Ok, so now that we've identified that we're a colonist feeding a prisoner,
+                //  we don't want to use any amazing foods in our inventory,
+                //  unless...
+                // 1) The best food in the inventory could actually be a nutrient paste meal.
+                //      If so, feed that to the prisoner scum!
+                // 2) We might not be able to find any paste meals or dispensers.
+                //      If so, then I guess we can feed the prisoner what we have in our inventory...
+                //      (but next time we're totally going to feed them paste)
+                Thing foodInInventory =
+                    FoodUtility.BestFoodInInventory_NewTemp(getter, null, FoodPreferability.MealAwful,
+                        FoodPreferability.MealLavish, 0f, false);
+                Thing canFindPaste = GetPasteSource(getter, eater);
+                if (foodInInventory != null
+                    && foodInInventory.def.defName != "MealNutrientPaste"
+                    && canFindPaste != null) {
                     canUseInventory = false;
                 }
-                
+
                 return true;
             }
         }
-        
-        [HarmonyPatch(typeof(FoodUtility))]
-        [HarmonyPatch("BestFoodSourceOnMap")]
-        private static class BestFoodSourceOnMap_Patch {
 
+        [HarmonyPatch(typeof(FoodUtility))]
+        [HarmonyPatch("BestFoodSourceOnMap_NewTemp")]
+        private static class BestFoodSourceOnMap_Patch {
             /**
-             * This prefix will return the nearest nutrient paste meal or dispenser iff this is part of a job for a warden or doctor to feed a prisoner.
+             * This prefix will return the nearest nutrient paste meal or dispenser if this is part of a job for a warden or doctor to feed a prisoner.
              */
             public static bool Prefix(ref Thing __result, Pawn getter, Pawn eater, ref ThingDef foodDef) {
-
-                Thing pasteSource = getPasteSource(getter, eater);
-
-                if (pasteSource != null) {
-                    __result = pasteSource;
-                    foodDef = FoodUtility.GetFinalIngestibleDef(pasteSource, false);
-                    return false;
-                }
+                Thing pasteSource = GetPasteSource(getter, eater);
 
                 // If we didn't find a food, then use the original method
-                return true;
+                if (pasteSource == null) return true;
+                __result = pasteSource;
+                foodDef = FoodUtility.GetFinalIngestibleDef(pasteSource, false);
+                return false;
             }
         }
     }
